b80698e13c11c5cbf3ec9cdf23c21ca52d9c9cf6
CASTREJON MEJIA MEJIA <104110369+ecas28@users.noreply.github.com>
2022-11-24 17:23:25 +0000

    fin

--- 3p/1recursive/2do/2.0problems.js
+++ 3p/1recursive/2do/2.0problems.js
@@ -1,12 +1,6 @@
 // SERIES
 // recursively
 //
-// 1. fibonacci
-// -> n 
-// <- 1,1,2,...,n
-//
-// call
-// fibonacci(5) returns the 5 first numbers of the series
 
 // 2. power
 // -> b base
@@ -23,25 +17,37 @@
 // call
 // factorial(3) returns 6
 
-function fib(n){
-    if (n < 2){ return n
-    }
-  return fib(n - 1) + fib(n - 2)
+
+// 1. fibonacci
+// -> n 
+// <- 1,1,2,...,n
+//
+// call
+// fibonacci(5) returns the 5 first numbers of the series
+function fibonacci(n){
+  if (n<2) return n
+  return fibonacci(n-2)+fibonacci(n-1)
 }
+
 function power(b,p) {
   if(p === 1) {
     return p
-  } else b * power(b, p - 1)
+  } else b * power(b, p - 1)
 }
+
 function factorial (n) {
    if (n == 0){
      return 1
      }
-     if (n == 1) {//base case
+     if (n == 1) {
         return n
     } else {
     
-    return  n * factorial( n - 1)
-    
-    }
+    return  n * factorial( n - 1)
+    
+    }
 }
+
+console.log((fibonacci(5))
+console.log((power(8))
+console.factorial((factorial(6))
